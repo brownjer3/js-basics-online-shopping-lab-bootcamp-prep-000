ad0aff265798d2f79d7a55abead2cc5e10818567
Jerry Brown <brownjer3@gmail.com>
2018-03-03 20:56:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -37,9 +37,7 @@ function viewCart() {
 
 function total() {
   for (var i = 0; i < cart.length; i++) {
-  var item = Object.keys(cart[i]);
-  var price = cart[i][item];
-  var total = total + price;
+  
   }
   return total;
 } 
