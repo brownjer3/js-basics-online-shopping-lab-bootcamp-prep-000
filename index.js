ba69556b080fd4bad99a5312a8b194a9089eaf47
Jerry Brown <brownjer3@gmail.com>
2018-03-03 20:17:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,9 +27,11 @@ function viewCart() {
     toPrint.push(item + " at $" + price);
   } if (cart.length === 0) {
     console.log("Your shopping cart is empty.");
-  } else if (cart.length <=2 ) {
+  } else if (cart.length <= 2 ) {
     console.log("In your cart, you have " + toPrint.join(" and ") + ".");
-  }
+  } else if (cart.length > 2 ) {
+    
+  } 
 }
   /* 
   if (cart.length === 0) {
