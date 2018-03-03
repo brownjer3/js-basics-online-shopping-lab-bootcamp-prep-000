8a560c480ff462e0e8b96da1694df3f775fb829c
Jerry Brown <brownjer3@gmail.com>
2018-03-03 20:34:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function viewCart() {
     //toPrint = toPrint.pop(); 
     var lastItem = toPrint.pop() ;
     var lastPrice = cart[i][lastItem];
-    console.log("In your cart, you have " + toPrint.join(", ") + " and " + lastItem + " at $" + lastPrice + ".");
+    console.log("In your cart, you have " + toPrint.join(", ") + " and " + toPrint.pop() ".");
   } 
 }
   /* 
