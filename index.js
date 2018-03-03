744581595ba5ade6f5fafb3bcd1f5cc8fa9ce96f
Jerry Brown <brownjer3@gmail.com>
2018-03-03 21:29:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function removeFromCart(item) {
   if (!item) {
     console.log("That item is not in your cart.");
   } else { 
-    var remove = Object.keys(cart[remove]);
+    var remove = Object.keys(cart[item]);
     delete cart[remove];
   }  
   return cart;
