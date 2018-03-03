77fbdfb01e900a05e6f2fada313b9ba803de5875
Jerry Brown <brownjer3@gmail.com>
2018-03-03 20:08:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,18 +23,14 @@ function viewCart() {
   var l = cart.length;
   if (cart.length === 0) {
    console.log("Your shopping cart is empty.");
- } else {
+ } else if (cart.length <= 2) {
    for (var i = 0; i < cart.length; i++) {
      var item = Object.keys(cart[i]);
      var price = cart[i][item];
        toPrint.push(item + " at $" + price);
   }
   console.log("In your cart, you have " + toPrint.join(", ") + ".");
-  } 
-}
-  
-  
-  /*else if (cart.length > 2) {
+  } else if (cart.length > 2) {
      var lastItem = Object.keys(cart[l]);
      var lastPrice = cart[l][lastItem];
    for (var p = 0; p < cart.length - 1 ; p++) {
