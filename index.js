02daa2bf9fa50fe750ad436118689679d6b5178a
Jerry Brown <brownjer3@gmail.com>
2018-03-03 20:26:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function viewCart() {
     console.log("In your cart, you have " + toPrint.join(" and ") + ".");
   } else if (cart.length >= 3 ) {
     toPrint = toPrint.slice(0, (toPrint.length)); 
-    var lastItem = Object.keys(cart[l]);
+    var lastItem = Object.keys(cart[l + 1]);
     var lastPrice = cart[l][lastItem];
     console.log("In your cart, you have " + toPrint.join(", ") + " and " + lastItem + " at $" + lastPrice + ".");
   } 
