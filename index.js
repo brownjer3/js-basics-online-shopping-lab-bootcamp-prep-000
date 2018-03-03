391619a73d199dd7edfa2ac1f9d6dbf537e6fd5c
Jerry Brown <brownjer3@gmail.com>
2018-03-03 21:53:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,11 +50,10 @@ function removeFromCart(item) {
    if (cart[i].hasOwnProperty(item)) {
      cart.splice(i, 1);
      return cart;
-  } else {
-    console.log("That item is not in your cart.");
-    return cart;
     }
   }
+  console.log("That item is not in your cart.");
+  return cart;
 }
 
 function placeOrder(cardNumber) {
